9935b3b6795cc9c051d59c25dcf0049e00d59728
meir <samberg31@gmail.com>
2022-03-28 13:36:32 +0300

    Exercises XP

---
+++ Week3/Day2/Exeercise2/script.js
@@ -0,0 +1,170 @@
+// EXERCISE 2
+
+// let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+// let myWatchedSeriesLength = myWatchedSeries.length ;
+// let myWatchedSeriesSentence = myWatchedSeries[0] + "" myWatchedSeries[1] +"" myWatchedSeries[2] ;
+
+//  console.log(myWatchedSeriesSentence("I watched" "series: " + myWatchedSeriesSentence);
+
+// let movies = ["black mirror", "money heist", "the big bang theory"];
+// movies[2] = "friends"
+// console.log(movies) //
+// movies.push("forest gump")
+// console.log(movies)
+
+// let movies = ["black mirror", "money heist", "the big bang theory"];
+// movies.shift()
+// console.log(movies)
+
+// EXERCISE 3
+
+// var cTemp = 30
+// var cFahr = cTemp * 9 / 5 + 32
+// console.log(cFahr)
+
+// EXERCISE 4
+
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction: 55
+// Actual: 55
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction: 23
+// Actual: 23
+
+// console.log(3 + 4 + `5`);
+
+
+// EXERCISE 5
+
+console.log(typeof 15)
+// Prediction: number
+// Actual: number
+
+console.log (typeof 5.5)
+// Prediction: number
+// Actual: number
+
+console.log (typeof NaN)
+// Prediction: not a number
+// Actual: number
+
+console.log (typeof "hello")
+// Prediction: word
+// Actual: string
+
+console.log (typeof true)
+// Prediction: boolean
+// Actual: boolean
+
+console.log (typeof 1 != 2)
+// Prediction:
+// Actual: true
+
+console.log ("hamburger" + "s")
+// Prediction: hamburgers
+// Actual: hamburgers
+
+console.log ("hamburgers" - "s")
+// Prediction: hamburger
+// Actual: NaN
+
+console.log ("1" + "3")
+// Prediction: 4
+// Actual: 13
+
+console.log ("1" - "3")
+// Prediction:
+// Actual: -2
+
+console.log ("johnny" + 5)
+// Prediction: NaN
+// Actual: johnny5
+
+console.log ("johnny" - 5)
+// Prediction:NaN
+// Actual: NaN
+
+console.log (99 * "hello")
+// Prediction: NaN
+// Actual: NaN
+
+console.log (1 != 1)
+// Prediction: true
+// Actual: false
+
+console.log (1 == "1")
+// Prediction: true
+// Actual: true
+
+ console.log (1 === "1")
+// Prediction: true
+// Actual: false
+
+// EXERCISE 6
+
+// What is the output of each of the expressions below?
+
+
+console.log (5 + "34")
+// Prediction: 39
+// Actual: 534
+
+console.log (5 - "4")
+// Prediction: 1
+// Actual: 1
+
+console.log (10 % 5)
+// Prediction: 2
+// Actual: 0
+
+console.log (5 % 10)
+// Prediction: 0
+// Actual: 5
+
+console.log ("Java" + "Script")
+// Prediction: JavaScript
+// Actual: JavaScript
+
+console.log(" " + " ")
+// Prediction:
+// Actual:
+
+console.log (" " + 0)
+// Prediction: 0
+// Actual: 0
+
+console.log (true + true)
+// Prediction: true
+// Actual: 2
+
+console.log (true + false)
+// Prediction: true
+// Actual: 1
+
+console.log (false + true)
+// Prediction: false
+// Actual: 1
+
+console.log (false - true)
+// Prediction: 0
+// Actual: -1
+
+console.log (!true)
+// Prediction: false
+// Actual: false
+
+console.log (3 - 4)
+// Prediction: -1
+// Actual: -1
+
+console.log ("Bob" - "bill")
+// Prediction: NaN
+// Actual: NaN
---
+++ Week3/Day2/Exercise1/script.js
@@ -0,0 +1,7 @@
+var x= "chocolate";
+var y= "dinner";
+
+
+console.log("I eat " + x + " at every " + y);
+
+
