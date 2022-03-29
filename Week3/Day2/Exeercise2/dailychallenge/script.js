6a5c45dbe91cb75a319ea6ce1a2d7e7a2b309b2e
meir <samberg31@gmail.com>
2022-03-29 12:01:49 +0300

    Day 2 completed

---
+++ Week3/Day2/Exeercise2/dailychallenge/script.js
@@ -0,0 +1,25 @@
+// Exercise 1
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift()
+console.log(fruits)
+
+fruits.sort()
+console.log(fruits)
+
+fruits.push("kiwi")
+console.log(fruits)
+
+fruits.splice (0, 1,)
+console.log(fruits)
+
+fruits.reverse()
+console.log(fruits)
+
+
+// Exercise 2
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(Oranges)
